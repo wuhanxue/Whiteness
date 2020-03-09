6d7865a9c59fc2ec58f8cc1a460c544eb6e75637
MurderousCrotch <1132464757@qq.com>
2020-03-09 22:35:01 +0900

    简单技能

---
+++ Assets/Scripts/OperationController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OperationController : MonoBehaviour {
+
+	public GameObject skill_1;
+	public GameObject skill_2;
+	private GameObject player;
+	private GameObject enemy;
+
+	void Awake()
+	{
+		player = GameObject.FindGameObjectWithTag("Player");
+		enemy = GameObject.FindGameObjectWithTag("Enemy");
+	}
+
+	public void OnAtkBtnClick()
+	{
+		player.GetComponent<Animation>().Play("player_move");
+		GameObject skillGo = Instantiate(skill_1, enemy.transform.position, Quaternion.identity);
+		Destroy(skillGo, 0.5f);
+	}
+
+	public void OnSklBtnClick()
+	{
+		player.GetComponent<Animation>().Play("player_move");
+		GameObject skillGo = Instantiate(skill_2, enemy.transform.position, Quaternion.identity);
+		Destroy(skillGo, 0.5f);
+	}
+
+	public void OnDefBtnClick()
+	{
+
+	}
+}
