5f9319a6babd05d85ceee0cd3d189330d82dfbc9
MurderousCrotch <1132464757@qq.com>
2020-04-05 17:34:44 +0900

    [Matt]对话设计

---
+++ Assets/Scripts/2-1_Dialog/PlayableTrackTalk.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.UI;
+
+// A behaviour that is attached to a playable
+public class PlayableTrackTalk : PlayableBehaviour
+{
+	public UILabel talkText1;
+
+	public string talkStr1;
+
+	// Called when the owning graph starts playing
+	public override void OnGraphStart(Playable playable) {
+		
+	}
+
+	// Called when the owning graph stops playing
+	public override void OnGraphStop(Playable playable) {
+		
+	}
+
+	// Called when the state of the playable is set to Play
+	public override void OnBehaviourPlay(Playable playable, FrameData info) {
+		talkText1.text = talkStr1;
+	}
+
+	// Called when the state of the playable is set to Paused
+	public override void OnBehaviourPause(Playable playable, FrameData info) {
+		
+	}
+
+	// Called each frame while the state is set to Play
+	public override void PrepareFrame(Playable playable, FrameData info) {
+		
+	}
+}
